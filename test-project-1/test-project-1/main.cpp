640aed96f3929c922bf391d3723a1600fa0bf504
Jake Tobiason <jake.thomas.tobiason@gmail.com>
2022-07-07 16:37:48 -0400

    origin commit

---
+++ test-project-1/test-project-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test-project-1
+//
+//  Created by Jake Tobiason on 7/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
